3ab6e0cd14e3f8cbebe3e32817d68fad3144d4fe
MikyPopescu <mihaellap42@gmail.com>
2020-11-07 13:26:41 +0200

    final

--- project/app.js
+++ project/app.js
@@ -1,7 +1,7 @@
 //API ESRI
 
 require(["esri/Map", "esri/views/MapView","esri/layers/FeatureLayer","esri/symbols/SimpleMarkerSymbol","esri/widgets/Legend", "esri/widgets/LayerList","esri/widgets/BasemapToggle", "esri/widgets/Search"], 
-  function (Map, MapView, FeatureLayer, SimpleMarkerSymbol,Legend, LayerList, BasemapToggle, Search) {
+  function (Map, MapView, FeatureLayer, _SimpleMarkerSymbol, Legend, LayerList, BasemapToggle, Search) {
 
     //obiect de tip harta
     let myMap = new Map({
@@ -20,11 +20,11 @@ require(["esri/Map", "esri/views/MapView","esri/layers/FeatureLayer","esri/symbo
      let symbolPharmacy = {
          type: "simple-marker",
          style: "square",
-         color:"blue",
-         size:"8px",
-         outline:{
-             color: [255,255,0],
-             width:3
+         color:"yellow",
+         size:"16px",
+         outline:{    
+             color: [0,100,0], //green
+             width:2
          }
      };
 
@@ -32,18 +32,17 @@ require(["esri/Map", "esri/views/MapView","esri/layers/FeatureLayer","esri/symbo
         type: "simple-marker",
         style: "triangle",
         color:"red",
-        size:"25px",
+        size:"16px",
         outline:{
             color: [0,0,255],
-            width:3
+            width:2
         }
     };
 
-    
-
+    //renderer
     const display = {
-        type: "unique-value",
-        field:"type",
+        type: "unique-value", 
+        field:"type",  //pharmacy/restaurant
         uniqueValueInfos:[{
             value:"restaurant",
             symbol: symbolRestaurant,
@@ -56,10 +55,6 @@ require(["esri/Map", "esri/views/MapView","esri/layers/FeatureLayer","esri/symbo
         }
     ]
     }
-
-
-
-
 
     let urlRegions = "https://services7.arcgis.com/eyYAdzn07jACFQSk/arcgis/rest/services/regions/FeatureServer/0"
     //add feature layer 
@@ -74,40 +69,9 @@ require(["esri/Map", "esri/views/MapView","esri/layers/FeatureLayer","esri/symbo
     //add to map multiple feature layers 
     myMap.addMany([flRegions, flPoints]);
 
-//Popup
-    let sablonPopUpRestaurantFarmacie = {
-        title: "{name}",
-        content: [
-          {
-            type: "fields",
-            fieldInfos: [
-              {
-                fieldName: "type",
-                label: "Denumire punct"
-              }
-            ]
-          }
-        ]
-      };
-      flPoints.popupTemplate = sablonPopUpRestaurantFarmacie;
-    
-    let sablonPopUpRegiuni = {
-        title: "{Regiune}",
-        content: [
-          {
-            type: "fields",
-            fieldInfos: [
-              {
-                fieldName: "reg_name",
-                label: "Nume Regiune"
-              }
-            ]
-          }
-        ]
-      };
-      flRegions.popupTemplate = sablonPopUpRegiuni;
 
-//legend
+
+    //widget legend
       const legenda = new Legend({
         view: viewMap,
         layerInfos: [
@@ -119,30 +83,65 @@ require(["esri/Map", "esri/views/MapView","esri/layers/FeatureLayer","esri/symbo
           }
         ]
       });
-      viewMap.ui.add(legenda, "bottom-right");
+      viewMap.ui.add(legenda, "bottom-left");
 
-//straturi tematice
+
+    //layer list
       const straturiTematice = new LayerList({
         view: viewMap
       });
       viewMap.ui.add(straturiTematice, "top-right");
 
-      const galerieHarti = new BasemapToggle({
-        view: viewMap,
-        nextBasemap: "topo-vector"
-      });
-      viewMap.ui.add(galerieHarti, "top-right");
 
 
-      //cautari
-      var surseCautare = [
+    //Popup
+    //template for interest points
+    let sablonPopUpRestaurantFarmacie = {
+      title: "Punct de interes {type}",
+      content: [
+        {
+          type: "fields",
+          fieldInfos: [
+            {
+              fieldName: "type",
+              label: "Tip punct de interes"
+            },
+            {
+              fieldName: "name",
+              label: "Denumire punct de interes"
+            }
+          ]
+        }
+      ]
+    };
+    flPoints.popupTemplate = sablonPopUpRestaurantFarmacie;
+  
+    //template for regions
+    let sablonPopUpRegiuni = {
+      title: "Regiunea {reg_name}",
+      content: [
+        {
+          type: "fields",
+          fieldInfos: [
+            {
+              fieldName: "reg_name",
+              label: "Denumire regiune"
+            }
+          ]
+        }
+      ]
+    };
+    flRegions.popupTemplate = sablonPopUpRegiuni;
+
+     //list of sources
+      let surseCautare = [
         {
           layer: flPoints,
           searchFields: ["name"],
           displayField: "name",
           exactMatch: false,
-          outFields: ["name"],
-          name: "Cautare restaurante/farmacii"
+          outFields: ["*"], //all columns are returned
+          name: "Cautare puncte de interes"
         },
        {
            layer: flRegions,
@@ -154,10 +153,19 @@ require(["esri/Map", "esri/views/MapView","esri/layers/FeatureLayer","esri/symbo
          }
       ];
 
-      var cauta = new Search({
+      //Search widget
+      let cauta = new Search({
         view: viewMap,
-        allPlaceholder: "Cauta restaurante,farmacii sau regiuni",
+        allPlaceholder: "Cauta restaurante, farmacii sau regiuni dupa denumire",
         sources: surseCautare
       });
+      
       viewMap.ui.add(cauta, "top-left");
+
+      //swap map types
+      const galerieHarti = new BasemapToggle({
+        view: viewMap,
+        nextBasemap: "hybrid"
+      });
+      viewMap.ui.add(galerieHarti, "top-right");
   });
